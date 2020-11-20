919c8db36c48304064c76991109e50a83510e5d4
Renee Enriquez <reneeenriquezmontalvan@gmail.com>
2020-11-20 00:06:16 -0500

    perfil

---
+++ app/src/main/java/com/app/appentrada/PerfilActivity.java
@@ -0,0 +1,21 @@
+package com.app.appentrada;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
